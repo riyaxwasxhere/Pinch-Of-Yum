e470350472f529ac2fb5cd239d09d884e4979461
Riya Dutta <riyapc1406@gmail.com>
2025-09-08 18:02:24 +0530

    Updated styling

--- src/components/FoodSlider.jsx
+++ src/components/FoodSlider.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import './FoodSlider.css'
+import { BrowserRouter, NavLink, Routes } from 'react-router-dom'
 const FoodSlider = (props) => {
   return (
     <div id='slider-card'>
-      <a href="#">
-        <img src={props.src} />
-        <span>play</span>
-        <h2>{props.name}</h2>
-      </a>
+      <BrowserRouter>
+        <nav>
+          <NavLink id="slider-nav" to="#">
+            <img src={props.src} />
+            <span>play</span>
+            <h2>{props.name}</h2>
+          </NavLink>
+        </nav>
+        <Routes>
+          {/* to be added later */}
+        </Routes>
+      </BrowserRouter>
+      
     </div>
   )
 }
