a91ce02f65749b77436b66a1e6b1ce504496f6a8
Riya Dutta <riyapc1406@gmail.com>
2025-09-08 19:04:43 +0530

    Updated styling

--- src/components/RecipeTypes.jsx
+++ src/components/RecipeTypes.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import './RecipeTypes.css'
+import { BrowserRouter, NavLink, Routes } from 'react-router-dom'
 const RecipeTypes = (props) => {
   return (
     <div id='rec-types'>
-      <a href="#">
-        <img src={props.src} />
-        <h3>{props.name}</h3>
-      </a>
+      <BrowserRouter>
+        <nav>
+          <NavLink id='rec-nav' to="#">
+            <img src={props.src} />
+            <h3>{props.name}</h3>
+          </NavLink>
+        </nav>
+        <Routes>
+          {/* to be added later */}
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
